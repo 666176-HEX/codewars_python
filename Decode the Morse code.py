259bbcaa542754219bdaa2dc115f50a522d69bc9
Александр <fedorov-av@ya.ru>
2021-10-09 21:31:24 +0300

    Decode the Morse code

---
+++ Decode the Morse code.py
@@ -0,0 +1,17 @@
+ENGLISH_TO_MORSE = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+                    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+                    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+                    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ':'|','':'',
+                    'SOS':'...---...','!':'-.-.--','.':'.-.-.-'}
+
+# Generate MORSE_TO_ENGLISH from ENGLISH_TO_MORSE
+MORSE_CODE = {}
+for key, value in ENGLISH_TO_MORSE.items():
+    MORSE_CODE[value] = key
+
+def decodeMorse(morse_code):
+    # ToDo: Accept dots, dashes and spaces, return human-readable message
+    return ('').join(MORSE_CODE[x] for x in morse_code.replace('   ',' | ').split(' ')).strip()
+
+print(decodeMorse('      ...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-  '))
